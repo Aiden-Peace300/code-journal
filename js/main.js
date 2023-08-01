dc784c7fa5895e5ff74eb6d3dc43b1fff2ad7d45
Aiden Peace <aidenpeace300@gmail.com>
2023-08-01 01:11:40 +0000

    Adding a submit event listener to the form element that will pervents the page from refreshing when the form is submitted also storing the form's input values in a new object called newEntry. incrementing the nextEntryid so that if another entry is submitted later, it will receive a different entryId. Adding a new object to the beginning of the data model's array entries. Resetting the image to the placeholder-image-square and then resetting the form

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
-const $photoUrlInput = document.querySelector('#photo-url-Input');
+const $photoUrlInput = document.querySelector('#photo-url-input');
 const $image = document.querySelector('#image');
+const $form = document.querySelector('#note-form');
+const entries = []; // Array to store the form data
+let nextEntryId = 1; // Initialize the nextEntryId
 
 function handlePhotoUrl(event) {
   const photoUrl = $photoUrlInput.value;
   $image.src = photoUrl;
 }
 
+function handleSubmit(event) {
+  event.preventDefault(); // Preventing form submission
+
+  const title = $form.elements.noteTitle.value;
+  const photoUrl = $form.elements.photoUrl.value;
+  const notes = $form.elements.message.value;
+
+  const newEntry = {
+    entryId: nextEntryId, // Assigning the entryId property from the nextEntryId variable
+    title,
+    photoUrl,
+    notes,
+  };
+
+  // Incrementing the nextEntryId for the next form submission
+  nextEntryId++;
+
+  // Adding the newEntry to the entries array
+  entries.unshift(newEntry);
+
+  // Resetting the preview image's src attribute back to the placeholder image
+  $image.src = 'images/placeholder-image-square.jpg';
+
+  // Resetting the form
+  $form.reset();
+}
+
 $photoUrlInput.addEventListener('input', handlePhotoUrl);
+$form.addEventListener('submit', handleSubmit);
