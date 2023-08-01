d0791359e8fdae207f75aa7a0d6c8529d6878777
Aiden Peace <aidenpeace300@gmail.com>
2023-08-01 02:30:32 +0000

    Completed User can create an entry requirements

--- js/data.js
+++ js/data.js
@@ -1,4 +1,8 @@
 /* exported data */
+// adding an event listener to the beforeunload event of the window object.
+// When the user is about to leave or reload the webpage,
+// the handleStorage function will be executed.
+window.addEventListener('beforeunload', handleStorage);
 
 let data = {
   view: 'entry-form',
@@ -6,3 +10,19 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+
+// getItem() => retrieve data from the browser's
+const previousEntriesJSON = localStorage.getItem('Javascript-local-storage');
+
+// Checking if previousEntriesJSON is not null and parse it if it has a value
+if (previousEntriesJSON !== null) {
+  data.entries = JSON.parse(previousEntriesJSON);
+}
+
+function handleStorage(event) {
+  // stringify() => static method converts a JavaScript value to a JSON string
+  const entriesJSON = JSON.stringify(data.entries);
+
+  // setItem() => store data in the browser's
+  localStorage.setItem('Javascript-local-storage', entriesJSON);
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const $photoUrlInput = document.querySelector('#photo-url-input');
 const $image = document.querySelector('#image');
 const $form = document.querySelector('#note-form');
-const entries = []; // Array to store the form data
-let nextEntryId = 1; // Initialize the nextEntryId
 
 function handlePhotoUrl(event) {
   const photoUrl = $photoUrlInput.value;
@@ -17,17 +15,17 @@ function handleSubmit(event) {
   const notes = $form.elements.message.value;
 
   const newEntry = {
-    entryId: nextEntryId, // Assigning the entryId property from the nextEntryId variable
+    entryId: data.nextEntryId, // Assigning the entryId property from the nextEntryId variable
     title,
     photoUrl,
     notes,
   };
 
   // Incrementing the nextEntryId for the next form submission
-  nextEntryId++;
+  data.nextEntryId++;
 
   // Adding the newEntry to the entries array
-  entries.unshift(newEntry);
+  data.entries.unshift(newEntry);
 
   // Resetting the preview image's src attribute back to the placeholder image
   $image.src = 'images/placeholder-image-square.jpg';
